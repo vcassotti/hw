0e55c25fee27dbda4c2f5ad42d6a945ed62772db
Vincent Cassotti <vcassotti@gmail.com>
2017-12-12 01:45:03 +0100

    add connection to pg

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,49 @@
-extern crate iron;
+#[macro_use]
+extern crate nickel;
 
-use iron::prelude::*;
-use iron::status;
+extern crate postgres;
+extern crate r2d2;
+extern crate r2d2_postgres;
+
+use std::env;
+use std::net::*;
+use std::fmt;
+use std::thread;
+use std::path::Path;
+
+use nickel::{Nickel, Router, HttpRouter, StaticFilesHandler};
+
+use r2d2_postgres::{TlsMode, PostgresConnectionManager};
 
 fn main() {
-    fn hello_world(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "Hello World!")))
-    }
 
-    let _server = Iron::new(hello_world).http("0.0.0.0:8080").unwrap();
-    println!("On 8080");
+    let conn_string:String = match env::var("POSTGRESQL_ADDON_URI") {
+        Ok(val) => val,
+        Err(_) => "postgres://cdvv:cdvv@localhost:5432/cdvv".to_string()
+    };
+
+    println!("{}", conn_string);
+
+    let manager = PostgresConnectionManager::new(conn_string, TlsMode::None).unwrap();
+    let pool = r2d2::Pool::new(manager).unwrap();
+    let conn = pool.get().unwrap();
+    let cwd:String = env::var("PWD").unwrap();
+    let mut server = Nickel::new();
+
+    server.utilize(StaticFilesHandler::new("public/"));
+    server.utilize(router());
+
+    server.listen("0.0.0.0:8080");
+}
+
+fn router() -> nickel::Router {
+
+    let mut router = Nickel::router();
+
+    router.get("/persons/:id", middleware! { |request|
+        let id = request.param("id").unwrap();
+        format!("read person '{}'", id)
+    });
+
+    router
 }
